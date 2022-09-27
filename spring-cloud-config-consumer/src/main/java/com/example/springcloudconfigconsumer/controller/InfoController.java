a6691658c57fbd987aa3b556e866eca3f304ecdf
panjh <panjh@eastime.com.cn>
2022-09-27 14:23:49 +0800

    springconfig配置中心消费端demo

--- spring-cloud-config-consumer/src/main/java/com/example/springcloudconfigconsumer/controller/InfoController.java
+++ spring-cloud-config-consumer/src/main/java/com/example/springcloudconfigconsumer/controller/InfoController.java
@@ -1,2 +1,17 @@
-package com.example.springcloudconfigconsumer.controller;public class InfoController {
+package com.example.springcloudconfigconsumer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @Value("${config.name:notFound}")
+    String name;
+
+    @PostMapping("/getConfig")
+    public String getConfig() {
+        return "name:" + name;
+    }
 }
