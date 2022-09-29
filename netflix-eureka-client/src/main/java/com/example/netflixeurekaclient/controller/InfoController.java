c8bda3927f4fe1b5ca4aaf07048124a6b50ea19f
panjh <panjh@eastime.com.cn>
2022-09-29 10:16:52 +0800

    暴露端口getinfo,为服务间调用做准备

--- netflix-eureka-client/src/main/java/com/example/netflixeurekaclient/controller/InfoController.java
+++ netflix-eureka-client/src/main/java/com/example/netflixeurekaclient/controller/InfoController.java
@@ -1,2 +1,13 @@
-package com.example.netflixeurekaclient.controller;public class InfoController {
+package com.example.netflixeurekaclient.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @PostMapping("/getInfo")
+    public String getInfo() {
+        return "eureka client 客户端方法回参";
+    }
 }
