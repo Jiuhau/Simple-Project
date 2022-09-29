94f3ccf4dfb53a2465984561da6a9b03b57564d2
panjh <panjh@eastime.com.cn>
2022-09-29 10:38:41 +0800

    杂项

---
+++ miscellaneous/api-test/src/main/java/com/example/apitest/java/api/date/DateTest.java
@@ -0,0 +1,10 @@
+package com.example.apitest.java.api.date;
+
+import java.util.Date;
+
+public class DateTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println(new Date(1664266740000l));
+    }
+}
