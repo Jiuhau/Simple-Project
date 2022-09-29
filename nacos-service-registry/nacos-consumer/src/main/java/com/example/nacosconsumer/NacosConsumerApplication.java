88672fee4de6ef07e8224476b9134de768bcf3f0
panjh <panjh@eastime.com.cn>
2022-09-29 10:12:39 +0800

    nacos注册中心、openfeign服务间调用demo

--- nacos-service-registry/nacos-consumer/src/main/java/com/example/nacosconsumer/NacosConsumerApplication.java
+++ nacos-service-registry/nacos-consumer/src/main/java/com/example/nacosconsumer/NacosConsumerApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//开启服务发现客户端 和@EnableEurekaClient注解一样功能 都是能够让注册中心能够发现，扫描到该服务。
-@EnableDiscoveryClient
-//启用feign客户端
-@EnableFeignClients
+@EnableDiscoveryClient // 开启服务发现客户端 和@EnableEurekaClient注解一样功能 都是能够让注册中心能够发现，扫描到该服务。
+@EnableFeignClients // 启用feign客户端
 @SpringBootApplication
 public class NacosConsumerApplication {
 
