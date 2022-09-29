7233c497b45aa1722dbdf802aee95209befb40d1
panjh <panjh@eastime.com.cn>
2022-09-29 10:24:17 +0800

    eureka服务消费者demo

---
+++ netflix-eureka-consumer/src/main/java/com/example/netflixeurekaconsumer/NetflixEurekaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.netflixeurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixEurekaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaConsumerApplication.class, args);
+    }
+
+}
