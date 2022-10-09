28e7921f52e6cf3152b67c65436404548710118b
panjh@eastime.com.cn <eastime@123>
2022-10-09 16:35:42 +0800

    zuul网关服务

--- netflix-zuul-server/src/main/java/com/example/netflixzuulserver/NetflixZuulServerApplication.java
+++ netflix-zuul-server/src/main/java/com/example/netflixzuulserver/NetflixZuulServerApplication.java
@@ -1,13 +1,27 @@
 package com.example.netflixzuulserver;
 
+import com.example.netflixzuulserver.filter.CustomZullFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy // 开启网关功能
 public class NetflixZuulServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetflixZuulServerApplication.class, args);
     }
 
+    /**
+     * 开启过滤器
+     *
+     * @return
+     */
+    @Bean
+    public CustomZullFilter preCustomZullFilter() {
+        return new CustomZullFilter();
+    }
 }
--- netflix-zuul-server/src/main/java/com/example/netflixzuulserver/filter/CustomZullFilter.java
+++ netflix-zuul-server/src/main/java/com/example/netflixzuulserver/filter/CustomZullFilter.java
@@ -1,2 +1,63 @@
-package com.example.netflixzuulserver.filter;public class CustomZullFilter {
+package com.example.netflixzuulserver.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+public class CustomZullFilter extends ZuulFilter {
+
+    /**
+     * 返回过滤器的类型。有pre、route、post、error等几种取值，分别对应上文的几种过滤器
+     * PRE：这种过滤器在请求被路由之前调用。可利用这种过滤器实现身份验证、在集群中选择请求的微服务、记录调试信息等。
+     * ROUTING：这种过滤器将请求路由到微服务。这种过滤器用于构建发送给微服务的请求,并使用Apache HttpClient或Netflix Ribbon请求微服务。
+     * POST：这种过滤器在路由到微服务以后执行。这种过滤器可用来为响应添加标准的 HTTP Header.收集统计信息和指标、将响应从微服务发送给客户端等。
+     * ERROR：在其他阶段发生错误时执行该过滤器。
+     * 除了默认的过滤器类型，Zuul还允许创建自定义的过滤器类型。例如，可以定制一种 STATIC类型的过滤器，直接在Zuul中生成响应，而不将请求转发到后端的微服务。
+     *
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    /**
+     * 返回一个int值来指定过滤器的执行顺序，不同的过滤器允许返回相同的数字。
+     * 数字越小优先度越高
+     *
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    /**
+     * 断该过滤器是否要执行，true表示执行, false表示不执行
+     *
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 过滤器的具体逻辑
+     *
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        return null;
+    }
 }
