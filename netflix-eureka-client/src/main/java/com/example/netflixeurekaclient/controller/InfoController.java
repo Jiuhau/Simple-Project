b92984cb63d165f6325eed51a11c3177fbcaf4ff
panjh@eastime.com.cn <eastime@123>
2022-10-11 17:39:42 +0800

    eureka服务提供者

--- netflix-eureka-client/src/main/java/com/example/netflixeurekaclient/controller/InfoController.java
+++ netflix-eureka-client/src/main/java/com/example/netflixeurekaclient/controller/InfoController.java
@@ -8,6 +8,7 @@ public class InfoController {
 
     @PostMapping("/getInfo")
     public String getInfo() {
+        System.out.println("我被调用啦!");
         return "eureka client 客户端方法getInfo()";
     }
 }
