112668016c307c7eb93a66339870230dbf2c552b
panjuhao <273452586@qq.com>
2023-02-13 13:42:39 +0800

    replace与replaceAll的使用

--- miscellaneous/api-test/src/main/java/com/example/apitest/java/lang/string/StringTest.java
+++ miscellaneous/api-test/src/main/java/com/example/apitest/java/lang/string/StringTest.java
@@ -2,6 +2,16 @@ package com.example.apitest.java.lang.string;
 
 public class StringTest {
     public static void main(String[] args) {
-
+        //split分割 但没有分割点
+        //答：不分割
+//        String a = "dealerCode";
+//        String[] splitResult = a.split("\\.");
+//        System.out.println(splitResult[0]);
+        //String.replace与String.replaceAll替换转义字符是否能识别
+        //答：replace可以不做特殊处理 replaceAll需要用正则表达式
+        String context = "服务顾问{}您好，预约车辆【{}】还有{}小时到达预约时间，请做好接待准备。";
+        System.out.println(context);
+        System.out.println(context.replace("{}","替换文本"));
+//        context.replaceAll("{0}","替换文学");
     }
 }
